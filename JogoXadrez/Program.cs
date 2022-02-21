0b58be33c115378dcd6dfe22dfbf2d3b783cd875
leo_b <leo_b@LAPTOP-RIO81TIC>
2022-02-21 18:14:14 -0400

    Criando primeira classe.

---
+++ JogoXadrez/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tabuleiro;
+
+namespace JogoXadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + P);
+        }
+    }
+}
